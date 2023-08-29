1a7aa8e80ca5dd8310f304dee95abbe3c2fa6527
Shoumq <lae345@mail.ru>
2023-08-29 21:55:31 +0300

    v2

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,6 @@
     <div class="header">
         <div class="container">
             <NuxtLink to="/" class="item">Главная</NuxtLink>
-            <NuxtLink to="/add_post" class="item">Добавить пост</NuxtLink>
         </div>
     </div>
 </template>
